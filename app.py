2ff8718ac4fb74c33059058c44b9ed24b6544bdd
Le Nguyen Gia Bao <lenguyengiabao46@gmail.com>
2022-01-17 17:56:28 +0700

    update color chart

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from pydoc import getpager
 import streamlit as st
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt 
 from PIL import Image
 import io
 import tensorflow as tf 
 import tensorflow.keras as keras
 from fashion_detection_model import SSD, Predictor
 from utils.utils import draw_boxes, cut_cothes
-from color_recognition import get_color
+from color_recognition import get_color, hex2name, rgb2hex
 from pattern_recognition import get_pattern
 st.set_page_config(page_title='Fashion Recognition',page_icon="👗", layout="wide")
-
 # @st.cache(hash_funcs={keras.engine.functional.Functional: id})
 # def load_model(model_path_detection, class_names_detection, model_path_pattern):
 #     net = SSD(len(class_names_detection), is_test=True)
@@ -49,6 +49,15 @@ def convert_color(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
+def plot_color(list_color, values):
+    ordered_colors = [list_color[i] for i in values.keys()]
+    hex_colors = [rgb2hex(ordered_colors[i]) for i in values.keys()]
+    name_color = [hex2name(ordered_colors[i]) for i in values.keys()]
+    fig1, ax1 = plt.subplots(figsize=(3,3))
+    ax1.pie(values.values(), labels = name_color, colors = hex_colors, startangle=90, textprops={'fontsize': 5})
+    ax1.axis('equal')  
+
+    return fig1, ax1
 
 def UI():
     st.markdown("<h1 style='text-align: center; color: black;'>Fashion Recognition</h1>", unsafe_allow_html=True)
@@ -69,15 +78,19 @@ def UI():
 
         _, a, b, _ = st.columns((1,3,7, 1))
         for i in list_clothes:
-            _, a, b, _ = st.columns((1,2,3, 1))
+            _, a, _, b, c,_ = st.columns((1,1,.1,1,1, 1))
             img_clothes = i['img']
             label_clothes = i['label']
-            color_clothes = get_color(img_clothes, 3)
+            center_colors, counts = get_color(img_clothes, 3)
+
             pattern_clothes = get_pattern(img_clothes, model_pattern, class_names_pattern)
             a.image(img_clothes)
-            b.markdown('## {}'.format(label_clothes))
-            b.markdown('### Color: {}'.format(color_clothes))
+            b.markdown('## Type: {}'.format(label_clothes))
+            # b.markdown('### Color: {}'.format(color_clothes))
             b.markdown('### Pattern: {}'.format(pattern_clothes))
+            c.markdown('### Color')
+            fig, ax = plot_color(center_colors, counts)
+            c.pyplot(fig)
             _, a, _ = st.columns((1,5, 1))
             a.markdown('---')
 
--- color_recognition.py
+++ color_recognition.py
@@ -6,6 +6,27 @@ import cv2
 from collections import Counter
 import webcolors
 
+def rgb2hex(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+
+
+def hex2name(color):
+    h_color = '#{:02x}{:02x}{:02x}'.format(int(color[0]), int(color[1]), int(color[2]))
+    try:
+        name = webcolors.hex_to_name(h_color, spec='css3')
+    except ValueError as v_error:
+        rms_lst = []
+        for img_clr, img_hex in webcolors.CSS3_NAMES_TO_HEX.items():
+            cur_clr = webcolors.hex_to_rgb(img_hex)
+            rmse = np.sqrt(mean_squared_error(color, cur_clr))
+            rms_lst.append(rmse)
+
+        closest_color = rms_lst.index(min(rms_lst))
+
+        name = list(webcolors.CSS3_NAMES_TO_HEX.items())[closest_color][0]
+    return name
+
+    
 def get_color(image, number_of_colors):
     h, w, c = image.shape
     modified_image = image.reshape(w*h, 3)
@@ -15,18 +36,17 @@ def get_color(image, number_of_colors):
     counts = Counter(labels)
     # sort to ensure correct color percentage
     counts = dict(sorted(counts.items()))
-    max_color_index = max(counts, key=counts.get)
-    center_colors = clf.cluster_centers_
-    color = center_colors[max_color_index]
-    rms_lst = []
-    for img_clr, img_hex in webcolors.CSS3_NAMES_TO_HEX.items():
-        cur_clr = webcolors.hex_to_rgb(img_hex)
-        rmse = np.sqrt(mean_squared_error(color, cur_clr))
-        rms_lst.append(rmse)
-    closest_color = rms_lst.index(min(rms_lst))
-    color_name = list(webcolors.CSS3_NAMES_TO_HEX.items())[closest_color][0]
 
-    return color_name
+    center_colors = clf.cluster_centers_
+
+    return center_colors, counts
+    # max_color_index = max(counts, key=counts.get)
+
+    # main_color = center_colors[max_color_index]
+    
+    # return main_color, center_colors, counts
+
+
 
 if __name__ == "__main__":
     img = cv2.imread('../image/1_crop.jpg')
